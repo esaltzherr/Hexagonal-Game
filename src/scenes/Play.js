a39d63c54eecdcd89adfcf3215f68875faf5d4e0
Elroy <lsaltzherr@gmail.com>
2022-07-26 17:20:03 -0700

    Selecting a Hex

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,13 +7,16 @@ class Play extends Phaser.Scene {
 
         this.load.image('hexfinder', './assets/hexfinder.png');
         this.load.tilemapTiledJSON('map', 'assets/Maps/HexGrid.json');
-        
+        this.load.image('highlight', 'assets/highlight.png');
+        this.load.image('select', 'assets/select.png');
+
         console.log('tiles');
 
 
     }
 
     create() {
+        this.input.mouse.disableContextMenu();
         this.map = this.make.tilemap({ key: 'map' });
         console.log("1");
         this.tileset = this.map.addTilesetImage('hexfinder');
@@ -22,21 +25,31 @@ class Play extends Phaser.Scene {
 
         console.log("1" + this.map);
 
-        this.marker = this.add.graphics();
-        this.marker.lineStyle(3, 0xffffff, 1);
-        this.marker.strokeRect(0, 0, this.map.tileWidth, this.map.tileHeight);
+        this.high = this.add.image(100, 100, 'highlight').setOrigin(0, 0);
+        this.selectedTileImage = this.add.image(0, 0, 'select').setOrigin(0, 0);
+        this.selectedTile = new Phaser.Math.Vector2();
+
+
+
+
 
         this.help = this.add.text(16, 500, 'Click on a tile to view its properties.', {
             font: '20px Arial',
             fill: '#ffffff'
         });
         this.help.setScrollFactor(0);
-        
+
         this.propertiesText = this.add.text(16, 540, 'Properties: ', {
             fontSize: '18px',
             fill: '#ffffff'
         });
 
+
+        this.selectedTile;
+        this.input.on('pointerdown', function (pointer) {
+            this.clicked();
+
+        }, this);
 
     }
     update() {
@@ -49,21 +62,20 @@ class Play extends Phaser.Scene {
 
         //console.log(this.map.HexagonalWorldToTileY(this.input.y));
         var test = this.map.getTileAtWorldXY(this.input.x, this.input.y);
-        if(test!=null){
-            console.log(test.x, test.y);
+        //console.log(test);
+        if (test != null) {
+
             this.pointerTileX = test.x;
             this.pointerTileY = test.y;
-
-
         }
         //console.log(test.x);
-        
+
         // console.log();
         // console.log(this.pointerTileX, this.pointerTileY);
 
 
         //console.log(Phaser.Math.RoundTo(this.pointerTile.x, 0),Phaser.Math.RoundTo(this.pointerTile.y, 0) );
-    
+
 
 
         // this.pointerTile = this.map.worldToTileXY(this.worldPoint.x, this.worldPoint.y);
@@ -72,36 +84,63 @@ class Play extends Phaser.Scene {
         // this.pointerTile.y = Phaser.Math.RoundTo(this.pointerTile.y, 0);
         //console.log(this.map.worldToTileXY(200,200));
 
-    
+
         //console.log(this.pointerTile.x, this.pointerTile.y);
 
 
         //console.log(this.worldPoint.x, this.worldPoint.y);
         // Snap to tile coordinates, but in world space
 
-        
+
         var between = this.map.tileToWorldXY(this.pointerTileX, this.pointerTileY);
         //console.log(between);
-        this.marker.x = between.x;
-        this.marker.y = between.y
+        this.high.x = between.x;
+        this.high.y = between.y;
+
+        //console.log(this.marker.x, this.input.x, this.marker.y, this.input.y);
 
         //console.log(this.pointerTile.x, this.marker.x, this.pointerTile.y, this.marker.y);
 
 
-        //console.log(this.marker.x, this.marker.y);
 
 
-        if (this.input.manager.activePointer.isDown) {
-            this.tile = this.map.getTileAt(this.pointerTileY, this.pointerTileY);
-
-            if (this.tile) {
-                // Note: JSON.stringify will convert the object tile properties to a string
-                this.propertiesText.setText('Properties: ' + JSON.stringify(this.tile.properties));
-                this.tile.properties.viewed = true;
-            }
-        }
 
     }
+    clicked() {
+        //console.log("Clicked");
+        this.tile = this.map.getTileAt(this.pointerTileX, this.pointerTileY);
+        console.log(this.tile);
+        if (this.tile) {
+            // Note: JSON.stringify will convert the object tile properties to a string
 
+            //this.tile.properties.selected = true;
+            console.log(this.selectedTile.x, this.pointerTileX);
+            if (!(this.selectedTile.x == this.tile.x) || !(this.selectedTile.y == this.tile.y)) {
+                //this.tile.properties.selected = true;
+
+                // console.log(this.high.x);
+                //this.selectedTile = this.tile;
+                this.selectedTile.x = this.tile.x;
+                this.selectedTile.y = this.tile.y;
+                // console.log(this.high.x);
+            }
+            else {
+                console.log("Unselect");
+                this.selectedTile.x = null;
+                this.selectedTile.y = null;
+                
+                
+            }
+
+            var between = this.map.tileToWorldXY(this.selectedTile.x,  this.selectedTile.y);
+            //console.log(between);
+            this.selectedTileImage.x = between.x;
+            this.selectedTileImage.y = between.y;
+
+
+
+            this.propertiesText.setText('Properties: ' + JSON.stringify(this.tile.properties) + this.pointerTileX + " - " + this.pointerTileY);
+        }
+    }
 
 }
